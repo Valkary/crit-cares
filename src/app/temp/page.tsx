f8d13879ee5aca625a87d8db70d6cc8a83642461
eselemu <119244920+eselemu@users.noreply.github.com>
2024-06-25 17:36:47 -0600

    +Page Edit View *Wrong temporal routing

---
+++ src/app/temp/page.tsx
@@ -0,0 +1,10 @@
+import { User, KeyRound, Ban, Check } from "lucide-react";
+import EditPatient from "~/components/patients/edit_patient";
+
+export default function Login({}) {
+    return (
+        <div className="dark min-h-screen flex items-center justify-center">        
+            <EditPatient />
+        </div>
+    );
+  }
